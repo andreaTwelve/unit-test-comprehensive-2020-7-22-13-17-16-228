fa0c6e165e052d9d446987c37297bb6471896845
andrea <andreatwelve@sina.cn>
2020-07-23 19:30:26 +0800

    [guessnumber] [andrea] [feature]: implement return 4A0B when number is 1234

--- src/main/java/example/GuessNumberGame.java
+++ src/main/java/example/GuessNumberGame.java
@@ -1,7 +1,21 @@
 package example;
 
 public class GuessNumberGame {
+    private String guessNumber;
+
+    public GuessNumberGame(String guessNumber) {
+        this.guessNumber = guessNumber;
+    }
+
     public String guessNumber(String guessNumber) {
-        return null;
+        return "4A0B";
+    }
+
+    public String getGuessNumber() {
+        return guessNumber;
+    }
+
+    public void setGuessNumber(String guessNumber) {
+        this.guessNumber = guessNumber;
     }
 }
--- src/test/java/example/GuessNumberTest.java
+++ src/test/java/example/GuessNumberTest.java
@@ -6,10 +6,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GuessNumberTest {
     @Test
-    void should_return_4A0B_when_number_is_1224_given_1234() {
+    void should_return_4A0B_when_number_is_1234_given_1234() {
         //given
         String guessNumber = "1234";
-        GuessNumberGame guessNumberGame = new GuessNumberGame();
+        GuessNumberGame guessNumberGame = new GuessNumberGame("1234");
 
         //when
         String actualResult = guessNumberGame.guessNumber(guessNumber);
