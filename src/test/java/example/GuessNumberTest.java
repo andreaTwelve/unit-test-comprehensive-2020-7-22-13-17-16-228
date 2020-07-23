d58e03f6417c24f740d3001ed472ecb7b0a7fbd9
andrea <andreatwelve@sina.cn>
2020-07-23 19:47:22 +0800

    [guessnumber] [andrea] [feature]: add should_return_0A2B_when_number_is_2107_given_1234 test

--- src/test/java/example/GuessNumberTest.java
+++ src/test/java/example/GuessNumberTest.java
@@ -56,4 +56,17 @@ public class GuessNumberTest {
         //then
         assertEquals("1A3B", actualResult);
     }
+
+    @Test
+    void should_return_0A2B_when_number_is_2107_given_1234() {
+        //given
+        String guessNumber = "2107";
+        GuessNumberGame guessNumberGame = new GuessNumberGame("1234");
+
+        //when
+        String actualResult = guessNumberGame.guessNumber(guessNumber);
+
+        //then
+        assertEquals("0A2B", actualResult);
+    }
 }
