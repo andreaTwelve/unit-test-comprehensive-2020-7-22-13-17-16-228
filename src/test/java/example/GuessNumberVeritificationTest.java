71a12335da0060a3a3e054012a964b06caf065c7
XIEAN2 <andreatwelve@sina.cn>
2020-07-24 10:29:27 +0800

    [guessnumber] [andrea] test: add should_return_false_when_number_is_12345_given_1234 test

--- src/test/java/example/GuessNumberVeritificationTest.java
+++ src/test/java/example/GuessNumberVeritificationTest.java
@@ -2,8 +2,7 @@ package example;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GuessNumberVeritificationTest {
     @Test
@@ -12,10 +11,20 @@ public class GuessNumberVeritificationTest {
         String guessNumber = "1 2 3 4";
         GuessNumberValidation validation = new GuessNumberValidation();
         //when
-        boolean isVirtual;
-        isVirtual = validation.checkLengthIsValidate(guessNumber);
+        boolean isValid;
+        isValid = validation.checkLengthIsValidate(guessNumber);
         //then
-        assertTrue(isVirtual);
+        assertTrue(isValid);
     }
 
+    @Test
+    void should_return_false_when_number_is_12345_given_1234() {
+        //given
+        String guessNumber = "1 2 3 4 5";
+        GuessNumberValidation guessNumberValidation = new GuessNumberValidation();
+        //when
+        boolean isValid = guessNumberValidation.checkLengthIsValidate(guessNumber);
+        //then
+        assertFalse(isValid);
+    }
 }
